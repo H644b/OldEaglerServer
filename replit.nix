cfd676fdb4aae300e8a95e06e5623308bf68acaa
H644b <timur.radjabov1@sascholar.org>
2024-10-24 19:52:33 -0400

    added my server

---
+++ replit.nix
@@ -0,0 +1,13 @@
+{ pkgs }: {
+    deps = [
+        pkgs.speedtest_cli
+        pkgs.speedtest_cli
+        pkgs.nano
+        pkgs.bashInteractive
+        pkgs.graalvm8-ce
+        pkgs.unzip
+        pkgs.nginx
+        pkgs.busybox
+        pkgs.tmux
+    ];
+}
